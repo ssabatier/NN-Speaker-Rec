bb774145ec9641a9f5119e1fc4fe9f61263f1549
ssabatier <34521882+ssabatier@users.noreply.github.com>
2018-07-31 23:16:30 -0400

    Update Gen_Genuine_pairs_Fn_Parallel.py

--- Gen_Genuine_pairs_Fn_Parallel.py
+++ Gen_Genuine_pairs_Fn_Parallel.py
@@ -8,37 +8,39 @@ import os
 import sys
 import itertools
 
-
 def Generate_Genuine(id, src_folder, dst_folder, choice_phase, choice_feature,
                      speech_aligned):
     """
     This function creates the genuine pairs based on the files created in "src_folder". Each file in "src_folder"
-    is a block of frames which forms a cube. For example if we created the block of 1sec duration of the sound file,
-    then each second has 98 frames and 40 features, each block in "src_folder" has (3,40,98) dimension. The naming
+    is a block of frames which forms a cube. For example if we created a block of 1sec duration, then each second
+    has 98 frames and 40 features, each block in "src_folder" has (3,40,98) dimensions. The naming
     convention for each file is preprocessed_choice_phase_choice_feature_cube_id_counter.
-    :param id: each pesron id.
-    :param src_folder: The folder which preprocessed file will be read from.
-    :param dst_folder: The destination folder which files will be saved there.
-    :param choice_phase: TRAIN or TEST
-    :param choice_feature: MFCC, logfilterbang_energy or etc.
-    :param speech_aligned: For creating genuine pairs, if each pair is built on the same speech words(the speaker utter
-    the in different sessions for example) then we are creating speech_aligned pairs otherwise the are speech_not_aligned and
-    the network is learning speaker information.
+    
+    args:
+    id: each person id.
+    src_folder: The folder which the pre-processed file will be read
+    dst_folder: The destination folder which the files will be saved
+    choice_phase: TRAIN or TEST
+    choice_feature: MFCC, log-filterbank_energy (MFEC), etc. 
+    speech_aligned: For creating genuine pairs, if each pair is built on the same speech words(the speaker utter
+                    the in different sessions for example) then we are creating speech_aligned pairs otherwise the 
+                    are speech_not_aligned and the network is learning speaker information.
 
-    :return: save the pairs. blocks are concatenated in the first axis. For example if each block if of size
-    (channels,features,frames) then the pairs are of size (channels,features,frames,2)
+    outputs:
+    Saved pairs. Blocks are concatenated in the first axis. For example if each block is of size
+    (channels,features,frames), then the pairs are of size (channels,features,frames,2)
     """
-    # TODO: Restriction of the upper bound of generating genuine pairs per id.
+    # Restriction of maximum of genuine pairs per id
     num_genuine_pairs = 30
 
-    # TODO: Generating Genuine pairs
+    # Generate Genuine pairs
     try:
         feature_names_all = glob.glob(os.path.join(src_folder, str(id),
-                                                   '*.npy'))  # Get the all numpy files in the source folder and its subfolders.
+                                                   '*.npy'))  # Get all numpy files in the source folder and its subfolders
         first_file = numpy.load(feature_names_all[0])
 
-        # number_blocks is the number of files in folder which for example is the number of 1s duration of data cube.
-        # ex: how many (3,40,98) block we have?(how many 1s is the frame length is 25ms with %60 overlap(10ms stride))
+        # number_blocks is the number of files in folder which for example are the number of 1s slices of the data cube
+        # ex: How many (3,40,98) blocks are there?
         number_blocks = len(feature_names_all)
 
         # Initialize the vector
@@ -46,25 +48,23 @@ def Generate_Genuine(id, src_folder, dst_folder, choice_phase, choice_feature,
 
         if speech_aligned == False:
             counter = 0
-            # TODO: Creating genuine pair based on the same file
-            # Check if the image belongs to the desired year of not.
+            # Create genuine pair based on the same file
+            # Check if the file belongs to the desired year or not
             for feature_name in feature_names_all:
                 feature[counter, :, :, :] = numpy.load(feature_name)
                 counter += 1
 
-            # Generate random vector.
+            # Generate random vector
             random_vector_1 = numpy.random.randint(number_blocks, size=(1, number_blocks))
             random_vector_2 = numpy.random.randint(number_blocks, size=(1, number_blocks))
 
             for i in range(num_genuine_pairs):
                 # WARNING: In order to create effective genuine pairs, the block of data must be non-overlapped!
-                # Get feature1_cube
                 left = feature[random_vector_1[0, i], :, :, :]
                 right = feature[random_vector_2[0, i], :, :, :]
-
                 pair = numpy.concatenate((left[:, :, :, None], right[:, :, :, None]), axis=3)
 
-                # Save by the appropriate naming convention.
+                # Save with the appropriate naming convention
                 numpy.save(os.path.join(dst_folder,
                                         choice_phase + '_' + choice_feature + '_' + 'gen' + '_' + str(
                                             id) + '_' + str(
@@ -87,13 +87,13 @@ def Generate_Genuine(id, src_folder, dst_folder, choice_phase, choice_feature,
                             # Increase the counter
                             counter += 1
 
-                            # Load the right part of the pair.
+                            # Load the right part of the pair
                             right = numpy.load(file_name_R)
 
-                            # Concatenate alongside the channel axis.
+                            # Concatenate alongside the channel axis
                             pair = numpy.concatenate((left[:, :, :, None], right[:, :, :, None]), axis=3)
 
-                            # Save by the appropriate naming convention.
+                            # Save by the appropriate naming convention
                             numpy.save(os.path.join(dst_folder,
                                                     choice_phase + '_' + choice_feature + '_' + 'gen' + '_' + str(
                                                         id) + '_' + str(
