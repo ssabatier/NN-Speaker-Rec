8fa7ff666b3fa49e3e81f8dd3fbcf8414325e075
ssabatier <34521882+ssabatier@users.noreply.github.com>
2018-08-01 00:57:04 -0400

    Update keras_speech_siamese_LSTM.py

--- keras_speech_siamese_LSTM.py
+++ keras_speech_siamese_LSTM.py
@@ -1,6 +1,4 @@
-## siamese LSTM using keras
-
-# # Investigate
+## Siamese LSTM using keras
 # np.random.seed(1337)  # for reproducibility
 
 import numpy as np
@@ -21,34 +19,26 @@ from sklearn.preprocessing import MinMaxScaler
 import h5py
 import sys
 
-
-
 def get_abs_diff(vects):
     x, y = vects
     return K.abs(x - y)
-
 
 def abs_diff_output_shape(shapes):
     shape1, shape2 = shapes
     return shape1
 
-
 def euclidean_distance(vects):
     x, y = vects
     return K.sqrt(K.sum(K.square(x - y), axis=1, keepdims=True))
-
 
 def l1_distance(vects):
     x, y = vects
     return K.abs(K.sum(x - y))
 
-
 def eucl_dist_output_shape(shapes):
     shape1, shape2 = shapes
     return (shape1[0], 1)
 
-
-# # # # # Commented by me ############
 # def contrastive_loss(y_true, y_pred):
 #     '''Contrastive loss from Hadsell-et-al.'06
 #     http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
@@ -65,93 +55,50 @@ def eucl_dist_output_shape(shapes):
 #     margin = 4.0
 #     return K.mean(y_true * K.square(K.minimum(margin, y_pred)) + (1 - y_true) * K.square(K.maximum(margin - y_pred, 0)))
 
-
-# def create_pairs(x, digit_indices, n_cat=10):
-#     '''Positive and negative pair creation.
-#     Alternates between positive and negative pairs.
-#     '''
-#     pairs = []
-#     labels = []
-#     n = min([len(digit_indices[d]) for d in range(n_cat)]) - 1
-#     if n==0:
-#     	print("Eliminate some classes")
-#     	cats = [len(digit_indices[d]) for d in range(n_cat)]
-#     	elim_list = [i for i,j in enumerate(cats) if j == 1]
-#     	cats = [i for j, i in enumerate(cats) if j not in elim_list]
-#     	n = min(cats) - 1
-#     	n_cat = len(cats)
-#     	digit_indices = [i for j, i in enumerate(digit_indices) if j not in elim_list]
-#     for d in range(n_cat):
-#         for i in range(n):
-#             z1, z2 = digit_indices[d][i], digit_indices[d][i+1]
-#             pairs += [[x[z1], x[z2]]]
-#             inc = random.randrange(1, n_cat)
-#             dn = (d + inc) % n_cat
-#             z1, z2 = digit_indices[d][i], digit_indices[dn][i]
-#             pairs += [[x[z1], x[z2]]]
-#             labels += [1, 0]
-#     return np.array(pairs), np.array(labels)
-
-
-# # # # # # # Commented by me ############
 # def compute_accuracy(predictions, labels):
 #     '''Compute classification accuracy with a fixed threshold on distances.
 #     '''
 #     return labels[predictions.ravel() < 0.5].mean()
-#
-# def compute_accu(labels, y_pred):
-#     ''''Compute classification accuracy with a fixed threshold on distances.'''
-#     #return K.mean(K.lesser(y_pred,K.ones(shape=(y_pred.get_shape()))))
-#     return K.mean([K.get_value(y_pred) < 0.5])
-
 
 def create_siamese_LSTM(input_dim):
     """
     Creating the architecture for siamese LSTM.
-
-    :param input_dim: Looks like it is not defined in this function!!!
+    :param input_dim: 
     :return: A (?,128) tensor as the output.
 
     PROCEDURE:
-
             1 - Create Keras Sequential model
             2 - Add layers one by one.
     """
     Sequential_Model = Sequential()
-
+    
     """
-    Add one dimensional convolutional models.
-
-    Notes:
-         * Activations are in the conv layers
-         * The last conv layer has activations too.
-
+    Add one dimensional convolutional models if desired
     """
     # Convolution-1
-    Sequential_Model.add(Convolution1D(filters=16, kernel_size=64, padding='valid', activation='relu',
-                                       strides=1, input_shape=(6400, 1)))
-    Sequential_Model.add(MaxPooling1D(pool_size=4))
-
-    # Convolution-2
-    Sequential_Model.add(Convolution1D(filters=32, kernel_size=32, padding='valid', activation='relu',
-                                       strides=1))
-    Sequential_Model.add(MaxPooling1D(pool_size=4))
-
-    # Convolution-3
-    Sequential_Model.add(Convolution1D(filters=64, kernel_size=16, padding='valid', activation='relu',
-                                       strides=1))
-    Sequential_Model.add(MaxPooling1D(pool_size=2))
-
-    # Convolution-4
-    # No pooling after last convolutional layer.
-    # There is still activation for this layer.
-    Sequential_Model.add(Convolution1D(filters=128, kernel_size=8, padding='valid', activation='relu',
-                                       strides=1))
-
+#    Sequential_Model.add(Convolution1D(filters=16, kernel_size=64, padding='valid', activation='relu',
+#                                       strides=1, input_shape=(6400, 1)))
+#    Sequential_Model.add(MaxPooling1D(pool_size=4))
+#
+#   # Convolution-2
+#  #  Sequential_Model.add(Convolution1D(filters=32, kernel_size=32, padding='valid', activation='relu',
+#                                      strides=1))
+#    Sequential_Model.add(MaxPooling1D(pool_size=4))
+#
+#    # Convolution-3
+#    Sequential_Model.add(Convolution1D(filters=64, kernel_size=16, padding='valid', activation='relu',
+#                                       strides=1))
+#    Sequential_Model.add(MaxPooling1D(pool_size=2))
+#
+#    # Convolution-4
+#    # No pooling after last convolutional layer.
+#    # There is still activation for this layer.
+#    Sequential_Model.add(Convolution1D(filters=128, kernel_size=8, padding='valid', activation='relu',
+#                                       strides=1))
     """
     Add LSTM layer.
     """
-    # The first LSTM returns its full output sequece.
+    # The first LSTM returns its full output sequence
     Sequential_Model.add(LSTM(128, return_sequences=True))
 
     # Dropout parameter.
@@ -162,27 +109,8 @@ def create_siamese_LSTM(input_dim):
 
     return Sequential_Model
 
-
-# digit_indices_tr=[]
-# # speakers verification
-# for speak in subj_tr:
-# 	digit_indices_tr.append([i for i, s in enumerate(ids) if speak in s ])
-# tr_pairs, tr_y = create_pairs(prova, digit_indices_tr, len(subj_tr))
-
-# digit_indices_te=[]
-# # speakers verification
-# for speak in subj_te:
-# 	digit_indices_te.append([i for i, s in enumerate(ids) if speak in s ])
-# te_pairs, te_y = create_pairs(prova, digit_indices_te, len(subj_te))
-
-# dataset=[tr_pairs, tr_y, te_pairs, te_y]
-# f=gzip.open('Speech_wav_stacked100_train_test.pkl.gz','wb')
-# cPickle.dump(dataset, f, protocol=cPickle.HIGHEST_PROTOCOL)
-# f.close()
-
-
 """
-Part1: Input the data.
+Part1: Input the data
 """
 SRC_FOLDER = '/home/stallone/Documents/PyCharm_Scripts/speech_processing-master/5-CreateData/HDF5/'
 hf_train = h5py.File(SRC_FOLDER + 'TRAIN_raw.hdf5', 'r')
@@ -192,10 +120,6 @@ tr_pairs = hf_train.get('pairs')
 tr_y = hf_train.get('labels')
 te_pairs = hf_test.get('pairs')
 te_y = hf_test.get('labels')
-
-# # The test subjects ids.
-# subj_tr = hf.get('subj_tr')
-# subj_te = hf.get('subj_te')
 
 # Training data shape of format of (Number of pairs(samples), 2, X1, X2)
 # X1: Probably temporal sequence, i.e., 100 successive frames.
@@ -207,18 +131,18 @@ te_pairs = np.transpose(te_pairs, (0, 2, 1, 3))
 print ("Training Pairs:", tr_pairs.shape)
 print ("Training shape:", tr_y.shape)
 
-# Turn into numpy arrays for processing.
+# Turn into numpy arrays for processing
 tr_pairs = np.array(tr_pairs)
 te_pairs = np.array(te_pairs)
 tr_y = np.array(tr_y)
 te_y = np.array(te_y)
 
-# Required parameters.
+# Required parameters
 input_dim = 1
 frames = 6400
 nb_epoch = 5
 
-# network definition
+# Network definition
 base_network = create_siamese_LSTM(input_dim)
 
 # Place holders.
@@ -231,22 +155,21 @@ input_b = Input(shape=(input_dim*frames, 1))
 processed_a = base_network(input_a)
 processed_b = base_network(input_b)
 
-
 # Of shape processed_a.get_shape() and processed_a.get_shape()
 abs_diff = Lambda(get_abs_diff, output_shape=abs_diff_output_shape)([processed_a, processed_b])
 
 # Of shape (?, 1)
 flattened_weighted_distance = Dense(1, activation='sigmoid')(abs_diff)
 
-# Create the keras model.
+# Create the keras model
 model = Model(inputs=[input_a, input_b], outputs=flattened_weighted_distance)
 
-# Otimization object.
+# Optimization object
 rms = RMSprop()
 sgd = SGD(lr=0.01)
 adam = Adam()
 
-# Model compiling.
+# Model compilation
 #model = load_model('my_model_48_epoch.h5')
 model.compile(loss='binary_crossentropy', optimizer=rms, metrics=['accuracy'])
 csv_logger = CSVLogger('training_rms_no_last_dense.log')
