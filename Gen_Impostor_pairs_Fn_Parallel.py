f6427eb45a2bf15dae410af0d07495fa24d38e49
ssabatier <34521882+ssabatier@users.noreply.github.com>
2018-07-31 23:18:20 -0400

    Update Gen_Impostor_pairs_Fn_Parallel.py

--- Gen_Impostor_pairs_Fn_Parallel.py
+++ Gen_Impostor_pairs_Fn_Parallel.py
@@ -8,13 +8,12 @@ import os
 import sys
 import itertools
 
-
 def Generate_Impostor(ID, id_set, relations, src_folder, dst_folder, choice_phase, choice_feature,
                       speech_aligned):
-    # Restrict the number of impostor pairs
+    # Restrict the max number of impostor pairs
     max_num_impostor_pairs = 30
 
-    # Find the left and right ids in the relations.
+    # Find the left and right ids in the relations
     id_left = str(ID)
     id_right = str(relations[int(numpy.where(relations[:, 0] == ID)[0][0]), 1])
     if id_left in id_set and id_right in id_set:
@@ -22,14 +21,14 @@ def Generate_Impostor(ID, id_set, relations, src_folder, dst_folder, choice_phas
         sound_left_all = glob.glob(os.path.join(src_folder, str(id_left), '*.npy'))
         sound_right_all = glob.glob(os.path.join(src_folder, str(id_right), '*.npy'))
 
-        # Looping over all files in the id folders.
+        # Loop over all files in the id folders
         counter = 0
         for sound_left_number, sound_left_name in enumerate(sound_left_all):
             for sound_right_number, sound_right_name in enumerate(sound_right_all):
 
                 if speech_aligned == False:
 
-                    # # Preventing repetition
+                    # Prevent repetition
                     if sound_left_number == sound_right_number and counter < max_num_impostor_pairs:
                         counter += 1
                         left = numpy.load(sound_left_name)
@@ -37,7 +36,7 @@ def Generate_Impostor(ID, id_set, relations, src_folder, dst_folder, choice_phas
 
                         pair = numpy.concatenate((left[:, :, :, None], right[:, :, :, None]), axis=3)
 
-                        # Save by the appropriate naming convention.
+                        # Save with the appropriate naming convention.
                         numpy.save(os.path.join(dst_folder,
                                                 choice_phase + '_' + choice_feature + '_' + 'imp' + '_' + str(
                                                     ID) + '_' + str(
