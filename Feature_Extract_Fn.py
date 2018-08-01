9e876225b7b729b59eb25ab248e0f18246ea47f8
ssabatier <34521882+ssabatier@users.noreply.github.com>
2018-07-31 22:13:50 -0400

    Update Feature_Extract_Fn.py

--- Feature_Extract_Fn.py
+++ Feature_Extract_Fn.py
@@ -7,17 +7,7 @@ from SPEECH_SIGNAL_PROCESSING import fbank
 from SPEECH_SIGNAL_PROCESSING import logfbank
 
 def ExtractFeature(signal,rate, feature_type):
-    """ This part is for HOG feature extraction******
-
-        If the choice_cube = True:
-             then the software creates a HOG cube based on the
-             vlfeat package which generate(?,?,31) dimensional HoG.
-             # Note: The default cell size in 8.
-        Otherwise:
-             the scikit_image package will be used.
-    """
-
-    """ This part is for detecting and cropping the face"""
+    
     if feature_type == 'fbank_energy':
         out_signal = fbank(signal, rate)
 
